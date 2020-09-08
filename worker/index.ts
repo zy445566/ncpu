05433d5c5e606e6b392424d77a61138294665de3
zy445566 <zyct@vip.qq.com>
2020-09-08 12:45:25 +0800

    used be kill

--- worker/index.ts
+++ worker/index.ts
@@ -9,7 +9,14 @@ const workerPath = pathJoin(__dirname, 'node-worker.js');
 export function runWorker (options:Object):Promise<any> {
     return new Promise((resolve, reject) => {
         const worker = new Worker(workerPath, options);
-        worker.on('message', resolve);
-        worker.on('error', reject);
+        worker.on('message', (res)=>{
+            resolve(res)
+            return worker.terminate()
+        });
+        worker.on('error', (err)=>{
+            reject(err);
+            return worker.terminate()
+        });
+        
     });
 }
